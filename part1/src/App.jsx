8776001939dd4546af905523f58f287a97463519
AgustinEscoriza <agusescoriza@outlook.es>
2024-10-04 19:26:45 -0300

    stopped in Complex state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
 
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
   )
+} 
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Display = ({ counter }) => {
   return (
     <div>{counter}</div>
   )
